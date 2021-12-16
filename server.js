4bb02df03643b6cbf3f7d10704504f890a835e24
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-16 15:54:03 -0800

    updated server.js, added routes/index.js

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const cors = require('cors');
-const logger = require('morgan');
-const PORT = process.env.PORT || 3001;
+const routes = require('./routes');
 const db = require('./db');
+
+// const cors = require('cors');
+// const logger = require('morgan');
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(logger('dev'));
+// app.use(cors());
+// app.use(express.json());
+// app.use(logger('dev'));
 
-app.get('/', (req, res) => {
-  res.send('This is root!');
-});
+app.use('/api', routes);
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// app.get('/', (req, res) => {
+//   res.send('This is root!');
+// });
 
 app.listen(PORT, () => {
-  console.log(`Express server listening on port ${PORT}`);
+  console.log(`Express server listening on port: ${PORT}`);
 });
