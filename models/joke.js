33d4773920f5d37a35e8f77f54bfb8029a24694c
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-16 11:29:55 -0800

    set up joke schema

---
+++ models/joke.js
@@ -0,0 +1,15 @@
+const { Schema } = require('mongoose');
+
+const Joke = new Schema(
+  {
+    setup: { type: String, required: true },
+    punchline: { type: String, required: true },
+    likes: { type: Number, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'users' },
+    seen_by: [{ type: Schema.Types.ObjectId, ref: 'users' }],
+    flagged: { type: Boolean, required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = Joke;
