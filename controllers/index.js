027d1b19ca334e638acd89f1d792c44c84951642
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-16 17:32:11 -0800

    added controller and route to get user by id

--- controllers/index.js
+++ controllers/index.js
@@ -19,6 +19,19 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (user) {
+      return res.status(200).json({ user });
+    }
+    return res.status(404).send('User with specified ID does not exist.');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
 // Post -->
 const createUser = async (req, res) => {
   try {
@@ -43,6 +56,7 @@ const createJoke = async (req, res) => {
 module.exports = {
   getAllJokes,
   getAllUsers,
+  getUserById,
   createUser,
   createJoke
 };
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = Router();
 router.get('/', (req, res) => res.send('This is root!'));
 router.get('/jokes', controllers.getAllJokes);
 router.get('/users', controllers.getAllUsers);
+router.get('/users/:id', controllers.getUserById);
 
 router.post('/users', controllers.createUser);
 router.post('/jokes', controllers.createJoke);
