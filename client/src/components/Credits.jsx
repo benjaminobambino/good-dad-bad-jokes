dda97f28fe9043f36ac864ca2267c8a8b10a1a4e
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-17 07:14:19 -0800

    updated site title and icon; started Credits component

---
+++ client/src/components/Credits.jsx
@@ -0,0 +1,7 @@
+const Credits = () => {
+  <div>
+    <div>Icon made by <a href="https://www.flaticon.com/authors/tulpahn" title="tulpahn">tulpahn</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+  </div>
+}
+
+export default Credits
