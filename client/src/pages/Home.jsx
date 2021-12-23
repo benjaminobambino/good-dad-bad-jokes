004f4b4a481f830768da2c075245c04c9dff2213
BenjaminoBambino <benjaminlpeck@gmail.com>
2021-12-23 09:59:14 -0800

    updated home text

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = (props) => {
   return(
     <div className='home' id={!props.launched ? 'home-background-animation' : null}>
       <h2>{welcomeMessage}</h2>
-      {!props.loggedIn ? (<div className={!props.launched ? 'opening-appearing' : null}><p>If you'd like to contribute your own humor by adding new jokes and "liking" current jokes, <Link to="/signup">create an account</Link> and <Link to="/login">log in</Link>. <br /><br /> Otherwise ...</p></div>) : null}
+      {!props.loggedIn ? (<div className={!props.launched ? 'opening-appearing' : null}><p>You don&#8217;t need to be a dad to enjoy dad jokes&mdash;good or bad. If you&#8217;d like to contribute your own humor by adding new jokes and &#8220;liking&#8221; current jokes, <Link to="/signup">create an account</Link> and <Link to="/login">log in</Link>. <br /><br /> Otherwise ...</p></div>) : null}
       <Link to="/jokes">
         <h4 className={!props.launched ? 'opening-appearing' : null}>Let the Jokes Begin</h4>
       </Link>
