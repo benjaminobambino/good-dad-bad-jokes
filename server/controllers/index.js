856340c72d7d616ad3feefd091e92873e47b2e20
Benjamin Peck <benjaminlpeck@gmail.com>
2022-05-16 10:54:00 -0700

    comment out createUser controller to allow for auth user POST

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,15 @@
 const { User, Joke } = require('../models');
 
 // POST -->
-const createUser = async (req, res) => {
-  try {
-    const user = await new User(req.body);
-    await user.save();
-    return res.status(201).json({ user });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-};
+// const createUser = async (req, res) => {
+//   try {
+//     const user = await new User(req.body);
+//     await user.save();
+//     return res.status(201).json({ user });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// };
 
 const createJoke = async (req, res) => {
   try {
