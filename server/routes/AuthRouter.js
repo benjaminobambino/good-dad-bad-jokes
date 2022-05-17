139a81181c8bfe2d9772829cfc32afe7c028b700
Benjamin Peck <benjaminlpeck@gmail.com>
2022-05-17 16:40:47 -0700

    add login auth route

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -3,5 +3,6 @@ const controllers = require('../controllers/AuthController');
 const router = Router();
 
 router.post('/signup', controllers.signup);
+router.post('/login', controllers.login);
 
 module.exports = router;
