a16df10bc2f7e137f9a0d935d894f9862ba9e083
Benjamin Peck <benjaminlpeck@gmail.com>
2022-05-18 01:55:32 -0700

    add middleware to routes that require login

--- server/routes/AppRouter.js
+++ server/routes/AppRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const controllers = require('../controllers/AppController');
+const middleware = require('../middleware');
 const router = Router();
 
-router.post('/jokes', controllers.createJoke);
+router.post('/jokes', middleware.isLoggedIn, controllers.createJoke);
 
 router.get('/', (req, res) => res.send('This is root!'));
 router.get('/jokes', controllers.getAllJokes);
@@ -10,10 +11,10 @@ router.get('/users', controllers.getAllUsers);
 router.get('/users/:id', controllers.getUserById);
 router.get('/jokes/flagged', controllers.getFlaggedJokes);
 
-router.put('/users/:id', controllers.updateUser);
-router.put('/jokes/:id', controllers.updateJoke);
+router.put('/users/:id', middleware.isLoggedIn, controllers.updateUser);
+router.put('/jokes/:id', middleware.isLoggedIn, controllers.updateJoke);
 
-router.delete('/users/:id', controllers.deleteUser);
-router.delete('/jokes/:id', controllers.deleteJoke);
+router.delete('/users/:id', middleware.isLoggedIn, controllers.deleteUser);
+router.delete('/jokes/:id', middleware.isLoggedIn, controllers.deleteJoke);
 
 module.exports = router;
