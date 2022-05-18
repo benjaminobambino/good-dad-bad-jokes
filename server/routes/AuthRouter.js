ff94e206c60082b7f79b2781420cbd330f39ad8d
Benjamin Peck <benjaminlpeck@gmail.com>
2022-05-18 02:04:58 -0700

    add checkSession route with isLoggedIn middleware

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const controllers = require('../controllers/AuthController');
+const middleware = require('../middleware');
 const router = Router();
 
 router.post('/signup', controllers.signup);
 router.post('/login', controllers.login);
+router.get('/session', middleware.isLoggedIn, controllers.checkSession);
 
 module.exports = router;
