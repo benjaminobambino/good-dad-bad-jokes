d5e68eac2998f3e87e20c95aa45999ef34c6ddd7
Benjamin Peck <benjaminlpeck@gmail.com>
2022-05-20 10:11:28 -0700

    remove console log from SignUp

--- client/src/forms/SignUp.jsx
+++ client/src/forms/SignUp.jsx
@@ -108,7 +108,6 @@ const SignUp = (props) => {
               'Passwords must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
           };
         } else {
-          console.log(state)
           return {
             ...state,
             valid: true,
