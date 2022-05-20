04772949785f36c3639bd2388b79a5c731eb1b51
Benjamin Peck <benjaminlpeck@gmail.com>
2022-05-20 09:57:15 -0700

    add RegisterUser to Auth services

---
+++ client/services/Auth.js
@@ -0,0 +1,8 @@
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/signup', data);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
