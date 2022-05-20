2c7c52025254ee5f7b13a475134b9e40a8055d92
Benjamin Peck <benjaminlpeck@gmail.com>
2022-05-20 09:51:35 -0700

    add api service with axios create and interceptor

---
+++ client/services/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const Client = axios.create({ baseURL: BASE_URL });
+// Intercepts every request axios makes
+Client.interceptors.request.use(
+  (config) => {
+    // Reads the token in localstorage
+    const token = localStorage.getItem('token');
+    // if the token exists, we set the authorization header
+    if (token) {
+      config.headers['authorization'] = `Bearer ${token}`;
+    }
+    return config; // We return the new config if the token exists or the default config if no token exists.
+    // Provides the token to each request that passes through axios
+  },
+  (error) => Promise.reject(error)
+);
+
+export default Client;
