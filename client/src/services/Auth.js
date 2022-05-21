1b560e3d0fe069f928b8046c2df972aa486cf2bf
Benjamin Peck <benjaminlpeck@gmail.com>
2022-05-21 07:23:43 -0700

    add CheckSession auth method

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -20,3 +20,12 @@ export const SignInUser = async (data, setMessageState) => {
     throw error;
   }
 };
+
+export const CheckSession = async () => {
+  try {
+    const res = await Client.get('/auth/session');
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
