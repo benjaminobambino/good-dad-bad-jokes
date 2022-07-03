d5f9935f18f3ea92d041a6b05d961bcb44aeebe1
Benjamin Peck <benjaminlpeck@gmail.com>
2022-07-02 18:48:02 -0700

    update props format in JokeCard

--- client/src/components/JokeCard.jsx
+++ client/src/components/JokeCard.jsx
@@ -4,39 +4,39 @@ import { JOKE_URL_PARAMS, USER_URL_PARAMS } from '../globals'
 import laugh from "../pics/laugh.png"
 import { useLocation } from 'react-router-dom'
 
-const JokeCard = (props) => {
+const JokeCard = ({ loggedIn, currentJoke, currentUser, setCurrentJoke, getUser, decrementJokes, incrementJokes }) => {
   const location = useLocation()
   const navButtons = location.pathname === '/jokes' ? true : false
 
   const [displayedMessage, setDisplayedMessage] = useState('')
 
-  const likedJoke = props.loggedIn && props.currentJoke ? props.currentUser.jokes_liked.includes(props.currentJoke._id) : null
+  const likedJoke = loggedIn && currentJoke ? currentUser.jokes_liked.includes(currentJoke._id) : null
 
   const toggleLiked = async () => {
     if (likedJoke) {
-      const index = props.currentUser.jokes_liked.findIndex((joke) => joke === props.currentJoke._id)
-      props.currentJoke.likes -= 1
-      props.setCurrentJoke({ ...props.currentJoke })
-      props.currentUser.jokes_liked.splice(index, 1)
-      await Client.put(`${USER_URL_PARAMS}/${props.currentUser._id}`, {
-        jokes_liked: props.currentUser.jokes_liked
+      const index = currentUser.jokes_liked.findIndex((joke) => joke === currentJoke._id)
+      currentJoke.likes -= 1
+      setCurrentJoke({ ...currentJoke })
+      currentUser.jokes_liked.splice(index, 1)
+      await Client.put(`${USER_URL_PARAMS}/${currentUser._id}`, {
+        jokes_liked: currentUser.jokes_liked
       }).then(() => {
-        props.getUser(props.currentUser._id)
+        getUser(currentUser._id)
       })
-      await Client.put(`${JOKE_URL_PARAMS}/${props.currentJoke._id}`, {
-        likes: props.currentJoke.likes
+      await Client.put(`${JOKE_URL_PARAMS}/${currentJoke._id}`, {
+        likes: currentJoke.likes
       })
     } else {
-      props.currentJoke.likes += 1
-      await Client.put(`${JOKE_URL_PARAMS}/${props.currentJoke._id}`, {
-        likes: props.currentJoke.likes
+      currentJoke.likes += 1
+      await Client.put(`${JOKE_URL_PARAMS}/${currentJoke._id}`, {
+        likes: currentJoke.likes
       })
-      props.currentUser.jokes_liked.push(props.currentJoke._id)
-      await Client.put(`${USER_URL_PARAMS}/${props.currentUser._id}`, {
-        jokes_liked: props.currentUser.jokes_liked
+      currentUser.jokes_liked.push(currentJoke._id)
+      await Client.put(`${USER_URL_PARAMS}/${currentUser._id}`, {
+        jokes_liked: currentUser.jokes_liked
       })
       .then(() => {
-        props.getUsers()
+        getUser(currentUser._id)
       })
     }
   }
@@ -58,24 +58,24 @@ const JokeCard = (props) => {
 
   return (
     <div>
-      {props.currentJoke ? (
+      {currentJoke ? (
         <div className="joke">
           
-          {navButtons ? <button onClick={props.decrementJokes}>Previous</button> : null}
+          {navButtons ? <button onClick={decrementJokes}>Previous</button> : null}
 
           <div className="joke-card">
-            <p className="setup">{props.currentJoke.setup}</p>
+            <p className="setup">{currentJoke.setup}</p>
             <div className="punchline-likes">
-              <p className="punchline">{props.currentJoke.punchline}</p>
-              <div className="likes-container" id={likedId} onClick={props.loggedIn ? toggleLiked : likeWhileLoggedOut}>
+              <p className="punchline">{currentJoke.punchline}</p>
+              <div className="likes-container" id={likedId} onClick={loggedIn ? toggleLiked : likeWhileLoggedOut}>
                 <img src={laugh} alt="laugh" className="laugh-image" />
-                <p>{props.currentJoke.likes}</p>
+                <p>{currentJoke.likes}</p>
               </div>
               <p>{displayedMessage}</p>
             </div>
           </div>
           
-          {navButtons ? <button onClick={props.incrementJokes}>Next</button>: null}
+          {navButtons ? <button onClick={incrementJokes}>Next</button>: null}
 
         </div>
       ) : null}
