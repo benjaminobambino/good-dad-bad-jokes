72bb70c6460a73223caf40184a17c0b9e9a50572
Benjamin Peck <benjaminlpeck@gmail.com>
2022-07-03 11:37:09 -0700

    add defaults to joke schema

--- server/models/joke.js
+++ server/models/joke.js
@@ -4,10 +4,10 @@ const Joke = new Schema(
   {
     setup: { type: String, required: true },
     punchline: { type: String, required: true },
-    likes: { type: Number, required: true },
+    likes: { type: Number, required: true, default: 0 },
     author: { type: Schema.Types.ObjectId, ref: 'users' },
     viewed_by: [{ type: Schema.Types.ObjectId, ref: 'users' }],
-    flagged: { type: Boolean, required: true }
+    flagged: { type: Boolean, required: true, default: false }
   },
   { timestamps: true }
 );
